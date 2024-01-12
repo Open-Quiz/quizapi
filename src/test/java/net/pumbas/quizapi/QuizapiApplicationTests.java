ffa494018d622dd2d95406a44805a2494aba5514
pumbas600 <joshjeffers600@gmail.com>
2024-01-12 18:23:41 +1100

    feat: :rocket: Initial project setup

---
+++ src/test/java/net/pumbas/quizapi/QuizapiApplicationTests.java
@@ -0,0 +1,13 @@
+package net.pumbas.quizapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
