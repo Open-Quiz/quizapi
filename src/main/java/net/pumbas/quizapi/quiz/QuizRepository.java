34419c19abbd334480219c260818b4bd096e082a
pumbas600 <joshjeffers600@gmail.com>
2024-02-09 23:28:53 +1100

    feat: :sparkles: Add query to count the number of questions in a quiz

--- src/main/java/net/pumbas/quizapi/quiz/QuizRepository.java
+++ src/main/java/net/pumbas/quizapi/quiz/QuizRepository.java
@@ -1,7 +1,10 @@
 package net.pumbas.quizapi.quiz;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface QuizRepository extends JpaRepository<Quiz, Long> {
 
+  @Query("SELECT SIZE(q.questions) FROM Quiz q WHERE q.id = ?1")
+  int countQuestionsById(Long quizId);
 }
