ec638ddbad5d69ff26c19ccc5bbf3deee3f269f2
pumbas600 <joshjeffers600@gmail.com>
2024-02-17 12:35:20 +1300

    feat: :sparkles: Add query for getting user by provider

--- src/main/java/net/pumbas/quizapi/user/UserRepository.java
+++ src/main/java/net/pumbas/quizapi/user/UserRepository.java
@@ -1,7 +1,13 @@
 package net.pumbas.quizapi.user;
 
+import java.util.Optional;
+import net.pumbas.quizapi.user.providers.UserDataProvider.Provider;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+  @Query("SELECT u FROM User u WHERE u.provider = ?1 AND u.providerId = ?2")
+  Optional<User> getUserByProvider(Provider provider, String providerId);
 
 }
