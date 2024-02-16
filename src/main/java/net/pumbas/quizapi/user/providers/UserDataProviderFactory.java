726d96c620ee67232685428543d70ae523e68230
pumbas600 <joshjeffers600@gmail.com>
2024-02-17 12:32:47 +1300

    refactor: :recycle: Update user data provider factory to use the provider names

--- src/main/java/net/pumbas/quizapi/user/providers/UserDataProviderFactory.java
+++ src/main/java/net/pumbas/quizapi/user/providers/UserDataProviderFactory.java
@@ -1,30 +1,34 @@
 package net.pumbas.quizapi.user.providers;
 
+import java.util.HashMap;
+import java.util.Map;
 import net.pumbas.quizapi.exception.UnauthorizedException;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserDataProviderFactory {
 
-  private final GoogleUserDataProvider googleUserDataProvider;
+  private final Map<String, UserDataProvider> providerMap = new HashMap<>();
 
   public UserDataProviderFactory(GoogleUserDataProvider googleUserDataProvider) {
-    this.googleUserDataProvider = googleUserDataProvider;
+    this.providerMap.put(googleUserDataProvider.getName().toString(), googleUserDataProvider);
   }
 
   /**
    * Factory method for getting the user data provider based on the case-insensitive provider name.
    * If the provider is not supported, an {@link UnauthorizedException} is thrown.
    *
-   * @param provider The case-insensitive name of the provider.
+   * @param providerName The case-insensitive name of the provider.
    * @return The {@link UserDataProvider} instance
    */
-  public UserDataProvider getProvider(String provider) {
-    return switch (provider.toLowerCase()) {
-      case "google" -> this.googleUserDataProvider;
-      default -> throw new UnauthorizedException(
-          "The user data provider is not supported: '%s'".formatted(provider));
-    };
+  public UserDataProvider getProvider(String providerName) {
+    UserDataProvider userDataProvider = this.providerMap.get(providerName.toUpperCase());
+    if (userDataProvider == null) {
+      throw new UnauthorizedException(
+          "The user data provider is not supported: '%s'".formatted(providerName));
+    }
+
+    return userDataProvider;
   }
 
 }
