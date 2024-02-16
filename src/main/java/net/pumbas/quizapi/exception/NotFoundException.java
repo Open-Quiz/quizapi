985393ade175f87bbd34d3747ab97d75569e6965
pumbas600 <joshjeffers600@gmail.com>
2024-02-17 11:31:20 +1300

    refactor: :recycle: Make exceptions extend `ResponseStatusException`

--- src/main/java/net/pumbas/quizapi/exception/NotFoundException.java
+++ src/main/java/net/pumbas/quizapi/exception/NotFoundException.java
@@ -1,8 +1,12 @@
 package net.pumbas.quizapi.exception;
 
-public class NotFoundException extends RuntimeException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+public class NotFoundException extends ResponseStatusException {
 
   public NotFoundException(String message) {
-    super(message);
+    super(HttpStatus.NOT_FOUND, message);
   }
+
 }
