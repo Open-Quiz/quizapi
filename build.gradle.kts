a4b671a03b46b98a5aa88af7631f065d9becc07f
pumbas600 <joshjeffers600@gmail.com>
2024-02-19 14:17:40 +1300

    fix: :bug: Add missing jjwt implementation dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,6 +32,7 @@ dependencies {
 
     implementation("io.jsonwebtoken:jjwt-api:0.12.5")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.5")
 
     compileOnly("org.projectlombok:lombok")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
