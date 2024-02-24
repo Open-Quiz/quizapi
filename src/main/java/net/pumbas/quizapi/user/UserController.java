b54513ca56f25347e24bc113c72dd65ba7ad29a8
pumbas600 <joshjeffers600@gmail.com>
2024-02-25 00:12:41 +1300

    feat: :sparkles: Add GET /api/v1/users/@me endpoint

--- src/main/java/net/pumbas/quizapi/user/UserController.java
+++ src/main/java/net/pumbas/quizapi/user/UserController.java
@@ -1,6 +1,10 @@
 package net.pumbas.quizapi.user;
 
 import net.pumbas.quizapi.config.Constants;
+import net.pumbas.quizapi.middleware.AuthenticationMiddleware.AuthContext;
+import net.pumbas.quizapi.middleware.annotations.Secured;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +12,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(Constants.API_V1 + "/users")
 public class UserController {
 
+  private final UserMapper userMapper;
+
+  public UserController(UserMapper userMapper) {
+    this.userMapper = userMapper;
+  }
+
+  @Secured
+  @GetMapping("/@me")
+  public ResponseEntity<UserDto> getMe() {
+    User requester = AuthContext.getCurrentUser();
+    return ResponseEntity.ok(this.userMapper.userDtoFromUser(requester));
+  }
 
 }
